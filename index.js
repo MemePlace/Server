25baca062011524b0be5d7cf98955b1655a00a51
Step7750 <Step7750@gmail.com>
2018-02-16 18:04:10 -0700

    Restructures Route Directory

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const config = require('./config.js');
 const express = require('express');
-const APIv1 = require('./routes/api.v1');
+const APIv1 = require('./routes/api/v1');
 const app = express();
 
 app.use('/api/v1', APIv1);
