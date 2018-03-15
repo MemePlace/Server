82fe11babf62a6ec709a2773d53910b1e8a60a56
Step7750 <Step7750@gmail.com>
2018-03-15 11:42:19 -0600

    Adds BodyParser

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const config = require('./config.js');
 const models = require('./models');
 const express = require('express');
+const bodyParser = require('body-parser');
 const APIv1 = require('./routes/api/v1');
 const app = express();
 
+app.use(bodyParser.json());
 app.use('/api/v1', APIv1);
 
 const port = config.port || 3000;
