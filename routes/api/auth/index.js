dc9f9d151f8fb188d4a8fe9580731b1c0d5e2e8b
Step7750 <Step7750@gmail.com>
2018-03-17 18:29:03 -0600

    Fixes isAuthenticated Entry Point

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -4,6 +4,17 @@ const models = require('../models');
 const router = express.Router();
 
 /**
+ * Middleware to check whether the user is authed
+ */
+router.isAuthenticated = function(req, res, next) {
+    if (req.session && req.session.userId && req.session.username && req.session.email) {
+        next();
+    } else {
+        res.status(401).json({error: 'You must be authenticated to interact with this resource'});
+    }
+};
+
+/**
  * Authenticate user credentials
  */
 router.post('/', async (req, res) => {
@@ -48,16 +59,5 @@ router.put('/logout', router.isAuthenticated, async (req, res) => {
         }
     });
 });
-
-/**
- * Middleware to check whether the user is authed
- */
-router.isAuthenticated = function(req, res, next) {
-    if (req.session && req.session.userId && req.session.username && req.session.email) {
-        next();
-    } else {
-        res.status(401).json({error: 'You must be authenticated to interact with this resource'});
-    }
-};
 
 module.exports = router;
