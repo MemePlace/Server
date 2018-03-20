faa2c27920a2346955abcce457e6f8483ee781ab
Step7750 <Step7750@gmail.com>
2018-03-20 15:21:01 -0600

    Fixes Max Length in Config

--- config.example.js
+++ config.example.js
@@ -14,5 +14,6 @@ module.exports = {
         saltRounds: 10,
     },
     allowedOrigins: ['http://localhost:4200', 'http://meme.place'],
-    ensureOrigin: false, // Whether to ensure the origin is in allowedOrigins (default DEV false, PROD true)
+    ensureOrigin: false, // Ensure origin is in allowedOrigins
+                         // (default DEV false, PROD true)
 };
