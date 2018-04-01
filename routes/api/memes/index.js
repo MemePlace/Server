2a7cfc2688f15862ad4eafbbdb7f2ffed7d70f2e
xiningchen <xiaoxining@hotmail.com>
2018-04-01 14:33:12 -0600

    meme id not auto incrementing

--- routes/api/memes/index.js
+++ routes/api/memes/index.js
@@ -27,42 +27,42 @@ router.post('/', auth.isAuthenticated, (req, res) => {
  * Gets lists of memes
  */
 router.get('/', async (req, res) => {
-    const sort = (['top', 'host', 'new'].includes(req.query.sort) && req.query.sort) || 'top';
-    const count = (0 < parseInt(req.query.count) && parseInt(req.query.count) < 100) ? parseInt(req.query.count) : 10;
-    // what is offset?
-    const offset = parseInt(req.query.offset) || 0;
-
-    // never defined an order??
-    let order;
-
-    if (sort === 'top') {
-        //order = ['voteCount', 'DESC']
-    }
-    else if (sort === 'new') {
-        // how are we storing creation time?
-        //order = ['createdAt', 'DESC'];
-    }
-    else if (sort === 'host'){
-        // get memes created by this user only
-        // not sure how to specify order...
-    }
-
-    // why do we need this total count?
-    const totalCount = await models.Meme.count();
-
-    const memes = await models.Meme.findAll({
-        limit: count,
-        offset,
-        order: [order]
-    });
-
-    res.json({
-        memes,
-        totalCount,
-        offset,
-        size: memes.length,
-        sort
-    });
+    // const sort = (['top', 'host', 'new'].includes(req.query.sort) && req.query.sort) || 'top';
+    // const count = (0 < parseInt(req.query.count) && parseInt(req.query.count) < 100) ? parseInt(req.query.count) : 10;
+    // // what is offset?
+    // const offset = parseInt(req.query.offset) || 0;
+    //
+    // // never defined an order??
+    // let order;
+    //
+    // if (sort === 'top') {
+    //     //order = ['voteCount', 'DESC']
+    // }
+    // else if (sort === 'new') {
+    //     // how are we storing creation time?
+    //     //order = ['createdAt', 'DESC'];
+    // }
+    // else if (sort === 'host'){
+    //     // get memes created by this user only
+    //     // not sure how to specify order...
+    // }
+    //
+    // // why do we need this total count?
+    // const totalCount = await models.Meme.count();
+    //
+    // const memes = await models.Meme.findAll({
+    //     limit: count,
+    //     offset,
+    //     order: [order]
+    // });
+    //
+    // res.json({
+    //     memes,
+    //     totalCount,
+    //     offset,
+    //     size: memes.length,
+    //     sort
+    // });
 });
 
 /**
@@ -70,10 +70,12 @@ router.get('/', async (req, res) => {
  */
 router.get('/:memeid', async (req, res) => {
     // use link to find the meme? Not sure how to use ID to find the meme..
-    const memelink = req.params.link;
+    const memeId = req.id;
 
     const meme = await models.Meme.findOne({
-        where: models.sequelize.where(models.sequelize.fn('lower', models.sequelize.col('link')), memelink.toLowerCase()),
+        where: {
+            id: memeId
+        },
         include: [{
             model: models.User,
             as: 'creator',
