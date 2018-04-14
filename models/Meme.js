c55d70eccafb09316bde58a516c74bb07a19eb45
xiningchen <xiaoxining@hotmail.com>
2018-04-13 20:17:16 -0600

    resolve conflict with master

--- models/Meme.js
+++ models/Meme.js
@@ -1,32 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
     const Meme = sequelize.define('Meme', {
         title: DataTypes.STRING,
-        link: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            default: '',
-            validate: {
-                notEmpty: {
-                    msg: 'The meme must have an URL'
-                },
-                isUrl: {
-                    msg: 'Your URL doesn\'t have a proper structure'
-                },
-                isUnique: function(link, done){
-                    Meme.findOne({
-                        where: sequelize.where(sequelize.fn('lower', sequelize.col('link')), link.toLowerCase()),
-                    }).then((link) => {
-                        if (!link) {
-                            done();
-                        }else{
-                            done(new Error('That meme url already exist'));
-                        }
-                    }).catch((err) => {
-                        done(err);
-                    });
-                }
-            }
-        },
         totalVote: {
             type: DataTypes.INTEGER,
             defaultValue: 0
